5ec3db584314178534eb250815149c2b550e0c4b
Khangnvd <khangnguyen.030204@gmail.com>
2023-10-14 21:19:18 +0700

    Login & Register UI

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,8 +1,69 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 function Login() {
   return (
-    <div>Login</div>
-  )
+    <>
+      <Box
+        sx={{
+          minWidth: "100vw",
+          minHeight: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#ddd",
+          padding: 10,
+        }}
+      >
+        <Card sx={{ minWidth: 600 }}>
+          <CardContent
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography variant="h4" textAlign={"center"} marginBottom={2}>
+              Login
+            </Typography>
+            <TextField
+              id="outlined-basic"
+              label="Username"
+              variant="outlined"
+              fullWidth
+              sx={{ marginBottom: 2 }}
+            />
+            <TextField
+              id="outlined-basic"
+              label="Password"
+              variant="outlined"
+              fullWidth
+            />
+            <Button variant="contained" fullWidth sx={{ my: 2 }}>
+              Login
+            </Button>
+            <Typography variant="body" textAlign={"center"}>
+              Don't have an account?
+            </Typography>
+            <Link to={"/register"} style={{ width: "100%" }}>
+              <Button variant="outlined" fullWidth sx={{ mt: 2 }}>
+                Register
+              </Button>
+            </Link>
+          </CardContent>
+        </Card>
+      </Box>
+    </>
+  );
 }
 
-export default Login
+export default Login;
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,8 +1,68 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Container,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { Link } from "react-router-dom";
 
 function Register() {
   return (
-    <div>Register</div>
-  )
+    <>
+      <Box
+        sx={{
+          minWidth: "100vw",
+          minHeight: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#ddd",
+          padding: 10,
+        }}
+      >
+        <Card sx={{ minWidth: 600 }}>
+          <CardContent
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography variant="h4" textAlign={"center"} marginBottom={2}>
+              Register
+            </Typography>
+            <TextField
+              fullWidth
+              id="outlined-basic"
+              label="Username"
+              variant="outlined"
+              sx={{ marginBottom: 2 }}
+            />
+            <TextField
+              fullWidth
+              id="outlined-basic"
+              label="Password"
+              variant="outlined"
+            />
+            <Button variant="contained" fullWidth sx={{ my: 2 }}>
+              Register
+            </Button>
+            <Typography variant="body" textAlign={"center"}>
+              Don't have an account?
+            </Typography>
+            <Link to={"/login"} style={{ width: "100%" }}>
+              <Button variant="outlined" fullWidth sx={{ mt: 2 }}>
+                Login
+              </Button>
+            </Link>
+          </CardContent>
+        </Card>
+      </Box>
+    </>
+  );
 }
 
-export default Register
+export default Register;
